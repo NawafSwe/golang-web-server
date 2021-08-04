a5a95935d81f5ce50b66a59880e5b94cd3d9c5c1
NawafSwe <nawaf.jr9@hotmail.com>
2021-08-04 21:56:08 +0300

    feat:server running and acppeting requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,27 @@
 package main 
 
+import (
+	"net/http"
+	"fmt"
+	// "time"
+	"log"
+)
 
+type Welcome struct{
+	Message string 
+	Time string
+}
+
+// server entry point 
 func main() {
-	
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello!")
+	});
+	// starting server 
+	fmt.Println("Listening on localhost:6600");
+	if err := http.ListenAndServe(":6600", nil); err != nil{
+			log.Fatal(err)
+	}
+
 }
